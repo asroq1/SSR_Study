b1cc4d5e0f54b08612b8ba7c30577a1678ca1306
marsoon <asroq98@gmail.com>
2021-07-20 17:59:44 +0900

    Update : fix br error

--- front/components/UserProfile.jsx
+++ front/components/UserProfile.jsx
@@ -5,40 +5,40 @@ import { useDispatch, useSelector } from 'react-redux'
 import { logOutRequestAction } from '../reducers/user'
 
 const UserProfile = () => {
-	const dispatch = useDispatch()
-	const { me, logOutLoading } = useSelector(state => state.user)
-	const onLogout = useCallback(() => {
-		dispatch(logOutRequestAction())
-	}, [])
+  const dispatch = useDispatch()
+  const { me, logOutLoading } = useSelector(state => state.user)
+  const onLogout = useCallback(() => {
+    dispatch(logOutRequestAction())
+  }, [])
 
-	return (
-		<>
-			<Card
-				actions={[
-					<div key="twit">
-						HOWLING
-						<br />
-						{me.Posts.length}
-					</div>,
-					<div key="followings">
-						팔로잉
-						<br />
-						{me.Followings.length}
-					</div>,
-					<div key="followers">
-						팔로워
-						{me.Followers.length}
-						<br />
-					</div>,
-				]}
-			>
-				<Card.Meta avatar={<Avatar>{me.nickname[0]}</Avatar>} title="jung" />
-				<Button onClick={onLogout} loading={logOutLoading}>
-					로그아웃
-				</Button>
-			</Card>
-		</>
-	)
+  return (
+    <>
+      <Card
+        actions={[
+          <div key="twit">
+            HOWLING
+            <br />
+            {me.Posts.length}
+          </div>,
+          <div key="followings">
+            팔로잉
+            <br />
+            {me.Followings.length}
+          </div>,
+          <div key="followers">
+            팔로워
+            <br />
+            {me.Followers.length}
+          </div>,
+        ]}
+      >
+        <Card.Meta avatar={<Avatar>{me.nickname[0]}</Avatar>} title="jung" />
+        <Button onClick={onLogout} loading={logOutLoading}>
+          로그아웃
+        </Button>
+      </Card>
+    </>
+  )
 }
 
 export default UserProfile
